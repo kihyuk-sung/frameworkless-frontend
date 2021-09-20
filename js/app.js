6a1656789cc85a02b2467171ec9f3060b1c343ca
K <kihyuk.sung@outlook.kr>
2021-09-20 12:20:01 +0900

    feat: State를 실제로 렌더링
    - faker.js을 이용하여 더미데이터 생성
    - view에서 렌더링
    - 실제 app에서 컨트롤러 역할을 해서 렌더링 실행
    - 이때, 렌더링은 window.requestAnimationFrame에서 실행함
    - 이것에서 실행하는 것이 무조건 좋다.
    - 리뷰는 다음과 같다.
    - view 함수 하나에서 모든 것을 조작한다. 이는 매우 지저분해지기 쉽다.
    - 아이템은 string으로 작업하고, count는 test 작업으로 세며, filter는
    classList로 관리한다.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 import performance from './performance.js';
+import getTodos from './getTodos.js';
+import view from './view.js';
 
 performance.init();
+
+const state = {
+  todos: getTodos(),
+  currentFilter: 'All'
+};
+
+const main = document.querySelector('.todoapp');
+
+window.requestAnimationFrame(() => {
+  const newMain = view(main, state);
+  main.replaceWith(newMain);
+});
