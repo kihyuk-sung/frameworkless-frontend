ea86527cf80f6baeadd72565c5edd462bc39dfdc
K <kihyuk.sung@outlook.kr>
2021-09-20 15:41:31 +0900

    feat: registry를 사용하는 컨트롤러
    - 컨트롤러 단에서 레지스트리에 뷰를 등록한다.
    - root를 렌더링한다.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 import performance from './performance.js';
 import getTodos from './getTodos.js';
-import view from './view.js';
+import todosView from './todos.js';
+import counterView from './counter.js';
+import filtersView from './filters.js';
+import registry from './registry.js';
 
 performance.init();
+
+registry.add('todos', todosView);
+registry.add('counter', counterView);
+registry.add('filters', filtersView);
 
 const state = {
   todos: getTodos(),
   currentFilter: 'All'
 };
 
-const main = document.querySelector('.todoapp');
-
 window.requestAnimationFrame(() => {
-  const newMain = view(main, state);
+  const main = document.querySelector('.todoapp');
+  const newMain = registry.renderRoot(main, state);
   main.replaceWith(newMain);
 });
