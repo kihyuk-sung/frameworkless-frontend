005f2d825ef2beb87bf0157edb93ac9ad343fd6e
K <kihyuk.sung@outlook.kr>
2021-09-23 18:03:33 +0900

    feat: 리스너 추가

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@ import filtersView from './view/filters.js';
 import registry from './registry.js';
 import applyDiff from './applyDiff.js';
 import appView from './view/app.js';
-import modelFactory from './model/model.js';
-
-const model = modelFactory();
+import stateFactory from './model/state.js';
 
 performance.init();
 
@@ -17,10 +15,22 @@ registry.add('todos', todosView);
 registry.add('counter', counterView);
 registry.add('filters', filtersView);
 
+const loadState = () => {
+  const serializedState = window.localStorage.getItem('state');
+
+  if (!serializedState) {
+    return;
+  }
+
+  return JSON.parse(serializedState);
+};
+
+const state = stateFactory(loadState());
+
 const {
   addChangeListener,
   ...events
-} = model;
+} = state;
 
 const render = (state) => {
   window.requestAnimationFrame(() => {
@@ -31,3 +41,12 @@ const render = (state) => {
 };
 
 addChangeListener(render);
+addChangeListener(state => {
+  Promise.resolve().then(() => {
+    window.localStorage.setItem('state', JSON.stringify(state));
+  });
+});
+
+addChangeListener(state => {
+  console.log(`Current State (${(new Date())})`, state);
+});
