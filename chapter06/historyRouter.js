28db5c56ba5483d2e0c58986752328836d34446a
K <kihyuk.sung@outlook.kr>
2021-09-23 14:27:09 +0900

    feat: historyRouter

---
+++ chapter06/historyRouter.js
@@ -0,0 +1,94 @@
+const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
+const URL_FRAGMENT_REGEXP = '([^\\/]+)';
+const TICKTIME = 250;
+
+const extractUrlParams = (route, windowHash) => {
+  const params = {};
+
+  if (route.params.length === 0) {
+    return params;
+  }
+
+  const matches = windowHash.match(route.testRegExp);
+
+  matches.shift();
+
+  matches.forEach((paramValue, index) => {
+    const paramName = route.params[index];
+    params[paramName] = paramValue;
+  });
+
+  return params;
+};
+
+export default () => {
+  const routes = [];
+  let notFound = () => { };
+  let lastPathname;
+
+  const router = {};
+
+  const checkRoutes = () => {
+    const { pathname } = window.location;
+
+    if (lastPathname === pathname) {
+      return;
+    }
+
+    lastPathname = pathname;
+
+    const currentRoute = routes.find(route => {
+      const { testRegExp } = route;
+      return testRegExp.test(pathname);
+    });
+
+    if (!currentRoute) {
+      notFound();
+      return;
+    }
+
+    const urlParams = extractUrlParams(currentRoute, pathname);
+
+    currentRoute.component(urlParams);
+  };
+
+  /**
+   * @param {String} fragment 
+   * @param {*} component 
+   * @returns 
+   */
+  router.addRoute = (fragment, component) => {
+    const params = [];
+
+    const parsedFragment = fragment
+      .replace(ROUTE_PARAMETER_REGEXP, (match, paramName) => {
+        params.push(paramName);
+        return URL_FRAGMENT_REGEXP;
+      })
+      .replace(/\//g, '\\/');
+
+    routes.push({
+      testRegExp: new RegExp(`^${parsedFragment}$`),
+      component,
+      params
+    });
+
+    return router;
+  };
+
+  router.setNotFound = cb => {
+    notFound = cb;
+    return router;
+  };
+
+  router.navigate = path => {
+    window.history.pushState(null, null, path);
+  };
+
+  router.start = () => {
+    checkRoutes();
+    window.setInterval(checkRoutes, TICKTIME);
+  };
+
+  return router;
+};
--- chapter06/index.js
+++ chapter06/index.js
@@ -1,4 +1,4 @@
-import createRouter from './router.js';
+import createRouter from './historyRouter.js';
 import createPages from './pages.js';
 
 const container = document.querySelector('main');
@@ -8,10 +8,10 @@ const pages = createPages(container);
 const router = createRouter();
 
 router
-  .addRoute('#/', pages.home)
-  .addRoute('#/list', pages.list)
-  .addRoute('#/list/:id', pages.detail)
-  .addRoute('#/list/:id/:anotherId', pages.anotherDetail)
+  .addRoute('/', pages.home)
+  .addRoute('/list', pages.list)
+  .addRoute('/list/:id', pages.detail)
+  .addRoute('/list/:id/:anotherId', pages.anotherDetail)
   .setNotFound(pages.notFound)
   .start();
 
