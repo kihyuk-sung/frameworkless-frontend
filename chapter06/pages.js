64fd8ff5da1f728405d3a224eabcd555e244d3fb
K <kihyuk.sung@outlook.kr>
2021-09-23 12:16:23 +0900

    chapter06: router pages

---
+++ chapter06/pages.js
@@ -0,0 +1,20 @@
+/** @param {HTMLElement} container */
+export default container => {
+  const home = () => {
+    container.textContent = 'This is Home page';
+  };
+
+  const list = () => {
+    container.textContent = 'This is List page';
+  };
+
+  const notFound = () => {
+    container.textContent = 'Page Not Found!';
+  };
+
+  return {
+    home,
+    list,
+    notFound
+  };
+};
